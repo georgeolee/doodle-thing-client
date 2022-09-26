50e6ed85e345518dec288fc0edeef756c8393869
georgeolee <geoliverlee@gmail.com>
2022-09-25 19:32:37 -0700

    env

--- src/getServerCanvasData.js
+++ src/getServerCanvasData.js
@@ -8,7 +8,8 @@ export async function getServerCanvasData(successHandler, errorHandler=null){
     console.log('fetching canvas data from server...')
 
     try{
-        const res = await fetch(process.env.REACT_APP_SERVER_URL + process.env.REACT_APP_SERVER_CANVAS_DATA_ROUTE)
+        // const res = await fetch(process.env.REACT_APP_SERVER_URL + process.env.REACT_APP_SERVER_CANVAS_DATA_ROUTE)
+        const res = await fetch(process.env.REACT_APP_SERVER_URL + 'canvas')
         const cdata = await res.json()
         successHandler(cdata)
     }catch(e){

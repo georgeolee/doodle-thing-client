977aa05df52de260465eb267e22fca9ac585e2e0
georgeolee <geoliverlee@gmail.com>
2022-09-28 14:45:11 -0700

    get query params

--- src/getServerCanvasData.js
+++ src/getServerCanvasData.js
@@ -1,15 +1,18 @@
 
 /**
  * 
- * @param {function(canvasData)} successHandler 
- * @param {function(Error)} errorHandler 
+ * @param {{
+ *      onSuccess: function,
+ *      onError?: function,
+ *      query?:{width?:number, height?:number}
+ * }} options 
  */
 export async function getServerCanvasData(options){
 
     const {
         onSuccess,  //callback to handle server response
         onError = null,    //error handler (optional)
-        query = {},      //key/value query params (optional)
+        query = {},      //GET query params (optional)
     } = options
 
     console.log('fetching canvas data from server...')
@@ -22,7 +25,6 @@ export async function getServerCanvasData(options){
 
         url.search = queryParams
 
-        // const res = await fetch(process.env.REACT_APP_SERVER_URL + 'canvas')
         console.log(`GET ${url.toString()}`)
         const res = await fetch(url.toString())
         const cdata = await res.json()

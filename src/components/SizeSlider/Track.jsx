a00f5286207767ba5e3a56446d78442a63ab52c3
georgeolee <geoliverlee@gmail.com>
2022-10-02 14:00:30 -0700

    mobile testing

--- src/components/SizeSlider/Track.jsx
+++ src/components/SizeSlider/Track.jsx
@@ -16,6 +16,11 @@ export function Track(props){
 
     return(
         <svg
+            style={{
+                width: '100%',
+                aspectRatio: width/height,
+                boxShadow: '0 0 10px 0 #f80'
+            }}
             viewBox={`0 0 ${width} ${height}`}
             xmlns="http://www.w3.org/2000/svg"  
             >
